d3431117830c4177d89c5dcd806a6af908525492
namsub166 <33636396+namsub166@users.noreply.github.com>
2018-02-02 13:24:12 -0500

    Update lilyHomework.java

--- Sorting/lilyHomework.java
+++ Sorting/lilyHomework.java
@@ -11,32 +11,32 @@ package HackerRank.Sorting;
  */
 
 import java.util.*;
-import java.io.*;
+
 
 public class lilyHomework {
 
-    static int lilysHomework(Integer[] arr) {
+	static int lilysHomework(Integer[] arr) {
         /*
-         * Using optimizition in combinatorics, it is possible to point out the beautiful array 
-         * happens when the array is sorted ascending or descending.
+         *  Using optimizition in combinatorics, it is possible to point out the beautiful array 
+         *  happens when the array is sorted ascending or descending
          */
         int n = arr.length;
         
         int ascendingSwaps = 0;
         int[] homeworkAscendingSorted = new int[n];
-        HashMap<Integer,Integer> ascending = new HashMap<Integer,Integer>();
+        HashMap<Integer,Integer> original = new HashMap<Integer,Integer>();
         
         int descendingSwaps = 0;   
         int[] homeworkDescendingSorted = new int[n];        
-        HashMap<Integer,Integer> descending = new HashMap<Integer,Integer>();
+        HashMap<Integer,Integer> unoriginal = new HashMap<Integer,Integer>();
         
         //Initialize arrays and maps
         for(int i = 0; i < n; i++)
         {
             homeworkAscendingSorted[i] = arr[i];
             homeworkDescendingSorted[i] = arr[i];
-            ascending.put(homeworkAscendingSorted[i],i);
-            descending.put(homeworkDescendingSorted[i],i);
+            original.put(homeworkAscendingSorted[i],i);
+            unoriginal.put(homeworkDescendingSorted[i],i);
         }
             
         Arrays.sort(arr); //Sort the input ascending    
@@ -46,10 +46,10 @@ public class lilyHomework {
             {
                 //swap the element from homework to the right position
                 int temp = homeworkAscendingSorted[i];
-                homeworkAscendingSorted[i] = homeworkAscendingSorted[ascending.get(arr[i])];
-                homeworkAscendingSorted[ascending.get(arr[i])] = temp;
+                homeworkAscendingSorted[i] = homeworkAscendingSorted[original.get(arr[i])];
+                homeworkAscendingSorted[original.get(arr[i])] = temp;
                 //Update index after swap
-                ascending.put(temp,ascending.get(arr[i]));
+                original.put(temp,original.get(arr[i]));
                 ascendingSwaps++;               
             }
         }
@@ -61,26 +61,25 @@ public class lilyHomework {
             {
                 //swap the element from homework to the right position
                 int temp = homeworkDescendingSorted[i];
-                homeworkDescendingSorted[i] = homeworkDescendingSorted[descending.get(arr[i])];
-                homeworkDescendingSorted[descending.get(arr[i])] = temp;
+                homeworkDescendingSorted[i] = homeworkDescendingSorted[unoriginal.get(arr[i])];
+                homeworkDescendingSorted[unoriginal.get(arr[i])] = temp;
                 //Update index after swap
-                descending.put(temp, descending.get(arr[i]));
+                unoriginal.put(temp, unoriginal.get(arr[i]));
                 descendingSwaps++;
             }
         }
         return (Math.min(ascendingSwaps,descendingSwaps));//Choose the smallest of the two possible smallest
     }
 
-    public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	int n = br.read();
-    	Integer[] arr = new Integer[n];
-    	for(int i = 0; i < n; i++) {
-    		arr[i] = br.read();
-    	}
-    	br.close();
-    	int result = lilysHomework(arr);
-    	System.out.println(result);
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Integer[] arr = new Integer[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+        int result = lilysHomework(arr);
+        System.out.println(result);
+        in.close();
     }
 }
-
